e7136b9d19624f1432c40343db5be671e84e250c
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-06-11 00:19:36 +0800

    Update background and footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer = () => {
             className="object-contain"
           />
           <p className="text-base text-gray-700">
-            Car Show 2023 <br />
+            Car Hub 2023 <br />
             All rights reserved &copy;
           </p>
         </div>
@@ -37,6 +37,17 @@ export const Footer = () => {
               </div>
             );
           })}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
         </div>
       </div>
     </footer>
