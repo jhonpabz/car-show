8b17bc8d46aaf91f39eba103106c5860ee90cb51
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-06-18 14:29:55 +0800

    Update params onchange of year or fuel.

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,12 +5,27 @@ import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
 
 export const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
+  const router = useRouter();
+
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLocaleLowerCase());
+
+    router.push(newPathName);
+  };
+
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
