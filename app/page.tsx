f2ce6e13fe369e2505bc79ef45e1c621d1f221b0
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-06-18 13:39:16 +0800

    Update custom filter UI.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
@@ -25,9 +26,10 @@ export default async function Home({ searchParams }) {
 
           <div className="home__filters">
             <SearchBar />
+
             <div className="home__filter-container">
-              <CustomFilter title="fuel" />
-              <CustomFilter title="year" />
+              <CustomFilter title="fuel" options={fuels} />
+              <CustomFilter title="year" options={yearsOfProduction} />
             </div>
           </div>
 
